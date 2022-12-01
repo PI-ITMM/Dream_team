3eff46d2d0670e43b1ca771bf4b3156ba78201f4
Dobrynia100 <dobrynya2001@gmail.com>
2022-12-01 21:32:31 +0300

    Геолокация+интеграция с апи

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 settings = {
     'CLIENT_ID':'123',
     'group_id':'216563568',
-    'TOKEN':'vk1.a.dmZI7jlB1S26qaZsBHxk1Wk9FyG5EHgtKBn2mkCIzhjKwnPHVCjKNOA2irS3-Ui_V4QesbQA-ojQDRQxWQUeYE5-5j7rLSppAEyrwQAKrIUMFJw5edMgM43T-P-LpabBHfrAt9VaFPze1KDzw9MVgNoa2oSdDloKEeJtBneviwBqmOf5k9MklElhEeLrHZieSyWTiD2o--jkUzUmpuMqsQ',
+    'TOKEN':'your token',
     'yan_key':'your token'
 
 }
