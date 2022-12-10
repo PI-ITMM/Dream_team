43a5f9dc4b4c23bf38f8c5686a662c70774b4c83
Dobrynia100 <58748374+Dobrynia100@users.noreply.github.com>
2022-12-10 16:52:41 +0300

    Фикс недели

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ settings = {
 }
 api={
     0:'https://api.weatherbit.io/v2.0/current?lat=56.3264816&lon=44.0051395&lang=ru&units=M&key=your token',
-    1:'https://api.weather.yandex.ru/v2/forecast/?lat=56.3264816&lon=44.0051395&lang=ru_RU&limit=3',
+    1:'https://api.weather.yandex.ru/v2/forecast/?lat=56.3264816&lon=44.0051395&lang=ru_RU&limit=7',
     2:'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/56.3264816,44.0051395/today?key=your token',
     'week':'https://api.weatherbit.io/v2.0/forecast/daily?lat=56.3264816&lon=44.0051395&lang=ru&units=M&key=your token',
 
